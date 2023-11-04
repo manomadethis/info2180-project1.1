e5f116d77c8a1b3e9e154bc4d251890477697bbe
Kimano Lambert <67758469+manomadethis@users.noreply.github.com>
2023-11-03 21:24:38 -0500

    Updated scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-/* Add your JavaScript to this file */
+/* Add your JavaScript to this file *//* Add your JavaScript to this file */
+window.addEventListener('load', function(){
+    let submitButton = document.querySelector('.newsletter button');
+    let message = document.querySelector('.message');
+    const emailField = document.getElementById('email');
+
+    submitButton.addEventListener('click', function(event) {
+        event.preventDefault();
+        let fieldValue = emailField.value;
+        if (fieldValue === '') {
+            message.innerHTML = "Please enter a valid email address";
+        }
+
+        else {
+            message.innerHTML = `Thank You! Your email address ${fieldValue} has been added`;
+            emailField.value = '';
+        }
+    });
+});
